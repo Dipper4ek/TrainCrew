53d190f84d23a097926af1bd897725ecb5371953
Dipper4ek <itworkermykola@gmail.com>
2025-12-11 18:20:15 +0100

    commit

--- TrainCrew/settings.py
+++ TrainCrew/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,6 +136,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'traincrew.verific@gmail.com'
-EMAIL_HOST_PASSWORD = 'evxxtxjyisgcobje'  # твій App Password без пробілів
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')  # твій App Password без пробілів
 
 
